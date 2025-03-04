a3337f0806fa9c0806999a039708166af8b9fb6d
M Rakib Ahmed <ahmedmrakib24@gmail.com>
2025-03-04 16:34:24 +0600

    third card added

--- src/components/Recipes.jsx
+++ src/components/Recipes.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
-
-
+import { TfiTimer } from "react-icons/tfi";
+import { BsFire } from "react-icons/bs";
 const Recipes = () => {
     const [recipes, setRecipes]=useState([]);
     useEffect(()=>{
@@ -9,23 +9,41 @@ const Recipes = () => {
         .then(data => setRecipes(data)
         )
     },[])
-    console.log(recipes);
     
     return (
         <div className="md:w-2/3">
-           <div className="grid grid-cols-1 gap-7 mb-10 lg:grid-cols-2">
+           <div className="grid grid-cols-1 gap-7 mb-10 lg:grid-cols-2 ">
             {
-                recipes.map(recipe => <div className="card bg-base-100  shadow-xl">
+                recipes.map(recipe => <div className="card bg-base-100  shadow-xl ">
                     <figure>
-                        <img className="w-full"
+                        <img  className="w-full p-7 "
                             src={recipe.recipe_image} />
                     </figure>
                     <div className="card-body">
                         <h2 className="card-title text-xl font-medium text-gray-700">{recipe.recipe_name}</h2>
                         <p className="text-[15px] text-gray-600">{recipe.short_description}</p>
                         <p className="border-b-2 text-gray-400 pt-10"></p>
-                        <div className="card-actions justify-end">
-                            <button className="btn btn-primary">Buy Now</button>
+                        <h3 className="text-xl font-semibold">Ingredients:{recipe.ingredients.length}</h3>
+                        <ul className="list-disc text-gray-500 ml-5">
+                            {recipe.ingredients.map((item) => (
+                                <li>{item}</li>
+                            ))}
+                        </ul>
+                        <div className="flex gap-4">
+                            <div className="flex items-center gap-3">
+
+                               <TfiTimer></TfiTimer>
+                                <span className="text-gray-500 ">30 minutes</span>
+                            </div>
+                            <div className="flex items-center gap-3">
+
+                               <BsFire></BsFire>
+                                <span className="text-gray-500 ">600 calories</span>
+                            </div>
+
+                        </div>
+                        <div className="card-actions ">
+                            <button className="btn btn-primary capitalize rounded-full">want to cook</button>
                         </div>
                     </div>
                 </div>)
